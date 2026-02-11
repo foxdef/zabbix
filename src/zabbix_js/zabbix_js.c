4359755aa34b050e99383f543d563abfc90a932f
Bogdan Bespalov <foxdef01@proton.me>
2026-02-11 05:05:36 +0300

    Add debugger and debugger_reattach cmdline options handling

--- src/zabbix_js/zabbix_js.c
+++ src/zabbix_js/zabbix_js.c
@@ -99,6 +99,9 @@ static char	shortopts[] = "s:i:p:hVl:t:w:d:r:";
 static char	shortopts[] = "s:i:p:hVl:t:w:";
 #endif
 
+static int main_argc = 0;
+static char **main_argv = NULL;
+
 /* end of COMMAND LINE OPTIONS */
 
 /* debugger options */
@@ -106,6 +109,7 @@ static char	shortopts[] = "s:i:p:hVl:t:w:";
 static int debugger = 0;
 static int debugger_reattach = 0;
 #endif
+/* end of debugger options*/
 
 static char	*read_file(const char *filename, char **error)
 {
@@ -145,6 +149,73 @@ static char	*read_file(const char *filename, char **error)
 	return data;
 }
 
+/*
+ *  Duktape heap lifecycle
+ */
+
+#if defined(ZABBIX_JS_DEBUGGER_SUPPORT)
+static duk_idx_t debugger_request(duk_context *ctx, void *udata, duk_idx_t nvalues) {
+	const char *cmd;
+	int i;
+
+	(void) udata;
+
+	if (nvalues < 1) {
+		duk_push_string(ctx, "missing AppRequest argument(s)");
+		return -1;
+	}
+
+	cmd = duk_get_string(ctx, -nvalues + 0);
+
+	if (cmd && strcmp(cmd, "CommandLine") == 0) {
+		if (!duk_check_stack(ctx, main_argc)) {
+			/* Callback should avoid errors for now, so use
+			 * duk_check_stack() rather than duk_require_stack().
+			 */
+			duk_push_string(ctx, "failed to extend stack");
+			return -1;
+		}
+		for (i = 0; i < main_argc; i++) {
+			duk_push_string(ctx, main_argv[i]);
+		}
+		return main_argc;
+	}
+	duk_push_sprintf(ctx, "command not supported");
+	return -1;
+}
+
+static void debugger_detached(duk_context *ctx, void *udata) {
+	fprintf(stderr, "Debugger detached, udata: %p\n", (void *) udata);
+	fflush(stderr);
+
+	/* Ensure socket is closed even when detach is initiated by Duktape
+	 * rather than debug client.
+	 */
+	duk_trans_socket_finish();
+
+	if (debugger_reattach) {
+		/* For automatic reattach testing. */
+		duk_trans_socket_init();
+		duk_trans_socket_waitconn();
+		fprintf(stderr, "Debugger reconnected, call duk_debugger_attach()\n");
+		fflush(stderr);
+#if 0
+		/* This is not necessary but should be harmless. */
+		duk_debugger_detach(ctx);
+#endif
+		duk_debugger_attach(ctx,
+		                    duk_trans_socket_read_cb,
+		                    duk_trans_socket_write_cb,
+		                    duk_trans_socket_peek_cb,
+		                    duk_trans_socket_read_flush_cb,
+		                    duk_trans_socket_write_flush_cb,
+		                    debugger_request,
+		                    debugger_detached,
+		                    NULL);
+	}
+}
+#endif
+
 /******************************************************************************
  *                                                                            *
  * Purpose: executes command (script in form of a text)                       *
@@ -181,15 +252,13 @@ static int	execute_script(const char *command, const char *param, int timeout, c
 		goto failure;
 	}
 
-	if (1) {
-#if defined(DUK_CMDLINE_DEBUGGER_SUPPORT)
-		fprintf(stderr, "Debugger enabled, create socket and wait for connection\n");
-		fflush(stderr);
+	if (debugger) {
+#if defined(ZABBIX_JS_DEBUGGER_SUPPORT)
+		zabbix_log(LOG_LEVEL_WARNING, "Debugger enabled, create socket and wait for connection");
 		duk_trans_socket_init();
 		duk_trans_socket_waitconn();
-		fprintf(stderr, "Debugger connected, call duk_debugger_attach() and then execute requested file(s)/eval\n");
-		fflush(stderr);
-		duk_debugger_attach(ctx,
+		zabbix_log(LOG_LEVEL_WARNING, "Debugger connected, call duk_debugger_attach() and then execute requested file(s)/eval");
+		duk_debugger_attach(es.env.ctx,
 		                    duk_trans_socket_read_cb,
 		                    duk_trans_socket_write_cb,
 		                    duk_trans_socket_peek_cb,
@@ -199,7 +268,6 @@ static int	execute_script(const char *command, const char *param, int timeout, c
 		                    debugger_detached,
 		                    NULL);
 #else
-		zbx_free(errmsg);
 		zabbix_log(LOG_LEVEL_WARNING, "option --debugger ignored, no debugger support");
 #endif
 	}
@@ -270,6 +338,9 @@ int	main(int argc, char **argv)
 	/* see description of 'optind' in 'man 3 getopt' */
 	int			zbx_optind = 0;
 
+	main_argc = argc;
+	main_argv = (char **) argv;
+
 	const char		*config_source_ip = NULL;
 
 	zbx_progname = get_program_name(argv[0]);
@@ -314,10 +385,10 @@ int	main(int argc, char **argv)
 
 				break;
 			case 'd':
-				// TODO: debugger impl
+				debugger = 1;
 				break;
 			case 'r':
-				// TODO: debugger reattach impl
+				debugger_reattach = 1;
 				break;
 			case 'h':
 				zbx_print_help(zbx_progname, help_message, usage_message, NULL);
